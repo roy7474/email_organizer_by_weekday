cefd5d0e333e2bf5d3f123dddb873323e17aed57
Roy Contreras Benitez <roy7474@gmail.com>
2023-08-11 19:05:37 -0400

    comments have been added for clarity

--- email_organizer_by_weekday.py
+++ email_organizer_by_weekday.py
@@ -1,16 +1,18 @@
 
+
 file_name = input('Enter the name of the file: ')
 
 fhand = open(file_name)
-counts = dict()
+counts = dict()                                 # add an empty dictionary
 for line in fhand:
-    words = line.split()
-    if len(words) < 3 or words[0] != 'From':
+    words = line.split()                        # separate the text into individual words
+    if len(words) < 3 or words[0] != 'From':    # Discard words smaller than 3 digits or that do not start with 'From'
         continue
     else:
-        if words[2] not in counts:
+        if words[2] not in counts:              #if the word is not in the dictionary, add it and assign value 1
             counts[words[2]] = 1
         else:
-            counts[words[2]] +=1
-
+            counts[words[2]] +=1                 # Else, Take the third word, which correspond to the day of the week, 
+#                                                #add it to the dictionary and assign value 1'''
 print(counts)
+
